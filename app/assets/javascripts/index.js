0ea32c3719890944a2927bd60fe57aa40443d5d3
Guillermo Leopold <gleopold@toptierlabs.com>
2016-09-12 10:34:18 -0300

    javascript code improvement

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,18 +1,31 @@
 status = 1
 
 var ready = function() {
-  $(window).keyup(handleKeypress2)
+  $(window).keyup(handleKeypressStart)
 }
-
 
 $(document).on('ready', ready);
 $(document).on('load', ready);
 
-
-var handleKeypress2 = function (){
+var handleKeypressStart = function (){
     if (status == 1) {
       $('#toptier-logo').fadeOut(1000);
       status++;
       loadPage();
     }
 }
+
+var loadPage = function () {
+    var context = $('#demo')[0].getContext('2d');
+    var ficha = $('#chip')[0];
+    var clock = $('#clockdiv')[0];
+    var canvasMove = $('#can')[0].getContext('2d');
+    var audio = document.createElement("audio");
+    audio.src = "../assets/music.mp3";
+
+    window.onkeyup = Game.handleKeypress;
+
+    Game.run(context, ficha, clock, canvasMove, audio);
+};
+
+
